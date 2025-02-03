69e90d449aa56bfea8ac958432a39fd2a8dcb895
ventada <m.ghahri2025@gmail.com>
2025-02-03 13:16:13 +0330

    kk

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
 import { useState } from "react";
 import RegistrationModal from "./components/Modal";
+import { Route, Routes } from "react-router-dom";
+import Telegram from "./page/Telegram";
 
 const App = () => {
   const [showModal, setShowModal] = useState(false);
   console.log("🚀 ~ App ~ showModal:", showModal);
   return (
-    <div>
-      <button
-        onClick={() => {
-          setShowModal(true);
-        }}
-      >
-        Show Modal
-      </button>
-
-      <RegistrationModal />
-    </div>
+    <Routes>
+      <Route path="/telegram" element={<Telegram />} />
+    </Routes>
   );
 };
 
---
+++ src/page/Telegram.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {};
+
+const Telegram = (props: Props) => {
+  return <div>Telegram</div>;
+};
+
+export default Telegram;
